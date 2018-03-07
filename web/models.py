1638edf8096c72268ddfa61a67a54dd9201af468
majid1400 <m.b1400v5@gmail.com>
2018-03-07 00:19:00 -0800

    income in models and some beatification

--- web/models.py
+++ web/models.py
@@ -6,6 +6,15 @@ from django.db import models
 # Create your models here.
 class Expense(models.Model):
     text = models.CharField(max_length=255)
-    data = models.DateTimeField()
+    date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+    def __str__(self):
+        return "{} - {} - {}".format(self.text,self.date,self.amount)
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+    def __str__(self):
+        return "{} - {} - {}".format(self.text,self.date,self.amount)
